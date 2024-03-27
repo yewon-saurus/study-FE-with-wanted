b6bb8cd550797b3f902c07c76824844e1bdebdf5
yewon-saurus <ywwwon01@gmail.com>
2024-03-27 21:56:40 +0900

    [practice] className, style, state

--- week1-2/test/src/App.js
+++ week1-2/test/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let titleStyle = {
+    color: "green",
+  };
+  let line = {
+    background: "green",
+    height: "2px",
+    border: "0",
+  };
+  let section = {
+    textAlign: "left",
+    padding: "30px",
+  };
+
+  const [greeting, setGreeting] = useState('안녕하세요!');
+
+  const changeText = () => {
+    setGreeting('반가워요!');
+    console.log(greeting);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 style={titleStyle}>{greeting}</h1>
+      </div>
+      <hr style={line}></hr>
+      <div style={section}>
+        <p>이름을 입력해주세요.</p>
+        <input />
+        <button type='button' onClick={changeText}>제출</button>
+      </div>
     </div>
   );
 }
