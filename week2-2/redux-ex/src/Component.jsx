775f0af00cc9477e79de0a39259665c3057c10da
yewon-saurus <ywwwon01@gmail.com>
2024-04-03 21:23:50 +0900

    [practice] useSelector 연습

--- week2-2/redux-ex/src/Component.jsx
+++ week2-2/redux-ex/src/Component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 // 고양이
 // 강아지
 // 홈
 
 const Cat = (props) => {
+    const myCatData = useSelector((state) => state);
+    // 어디까지 가져올거니?.. state 다 가져오세요. 라고 한 셈
+    // const myCatData = useSelector((state) => state.cat.cat_arr);
+
     return (
         <div>
             <h1>App이 줬어요: {props.a}</h1>
             <h1>고양이</h1>
+            <div>
+                {
+                    myCatData.cat.cat_arr.map((ele, idx) => 
+                        <div key={idx}>
+                            {ele}
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 }
