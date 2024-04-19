54a1988d2fe61b775c2ea7838bffc7d03a10d01b
yewon-saurus <ywwwon01@gmail.com>
2024-04-19 21:08:25 +0900

    [study] React.memo (리렌더링 방지)

--- week4-3/memo-ex/src/App.js
+++ week4-3/memo-ex/src/App.js
@@ -17,6 +17,56 @@ const MemoComponent = ({a, b, c}) => {
   );
 }
 
+const ParentsComponent = () => {
+  const [name, setName] = React.useState('perl');
+
+  return (
+    <div>
+      <h1>부모 컴포넌트</h1>
+      <ChildOne name={name} />
+      <ChildTwo />
+
+      <p>{name}</p>
+      <button onClick={() => {
+        setName(name + 'a');
+      }}>이름 바꾸기</button>
+    </div>
+  );
+}
+
+const ChildOne = ({name}) => {
+  console.log('ChildOne 렌더링');
+
+  return (
+    <div>
+      <h2>자식1 컴포넌트</h2>
+
+    </div>
+  );
+}
+
+const ChildTwo = React.memo(() => {
+  console.log('ChildTwo 렌더링');
+
+  return (
+    <div>
+      <h2>자식2 컴포넌트</h2>
+      <ChildThree />
+    </div>
+  );
+})
+
+const ChildThree = () => {
+  console.log('ChildThree 렌더링');
+
+  return (
+    <div>
+      <h2>자식3 컴포넌트</h2>
+
+    </div>
+  );
+}
+
 function App() {
   const [numbers, setNumbers] = React.useState({
     a: 0,
@@ -26,13 +76,21 @@ function App() {
 
   return (
     <div className="App">
-      <MemoComponent a={numbers.a} b={numbers.b} c={numbers.c} />
-      <button onClick={() => {
-        setNumbers({...numbers, a: numbers.a + 1, b: numbers.b + 1});
-      }}>a, b를 변경해요</button>
-      <button onClick={() => {
-        setNumbers({...numbers, c: numbers.c + 1});
-      }}>c를 변경해요</button>
+      <div>
+        <MemoComponent a={numbers.a} b={numbers.b} c={numbers.c} />
+        <button onClick={() => {
+          setNumbers({...numbers, a: numbers.a + 1, b: numbers.b + 1});
+        }}>a, b를 변경해요</button>
+        <button onClick={() => {
+          setNumbers({...numbers, c: numbers.c + 1});
+        }}>c를 변경해요</button>
+      </div>
+
+      <hr />
+
+      <div>
+        <ParentsComponent />
+      </div>
     </div>
   );
 }
